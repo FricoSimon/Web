cc1b35ca748803a53388b9f28d8e4d9aed12af87
FricoSimon <simonfriko@gmail.com>
2023-08-07 16:28:26 +0700

    add register route logic database - success

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql';
 const db = mysql.createConnection({
-    host: '172.17.0.4',
+    host: '172.17.0.3',
     port: '3306',
     user: 'root',
     password: 'web',
--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,30 @@ import db from '../database/connect.js';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/api/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const sql = `INSERT INTO user (username, password) VALUES (?, ?)`;
+        const values = [username, password];
+        const result = await new Promise((resolve, reject) => {
+            db.query(sql, values, (err, result) => {
+                if (err) reject(err)
+                resolve(result)
+            });
+        })
+
+        res.status(201).json({ message: 'Register success' });
+    } catch (error) {
+        console.log(req.body);
+        console.log(error);
+    }
 });
 
 
